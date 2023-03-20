428e9327634b141bffe2bab38953858b570f5be2
JustinLee21 <JLLemmons21@gmail.com>
2023-03-20 09:22:53 -0400

    Project: cooking_project, created all of the components that will be interacting w/ each other for this application.

--- cooking_project/src/app/recipe/recipe-list/recipe-list.component.ts
+++ cooking_project/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipe: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', "https://1.bp.blogspot.com/-e-GOmN5ct00/X1a5uEU4HrI/AAAAAAAAYsA/mh_OPlhyyv0RUGcCXTr_hh0W_sAkvf1HwCLcBGAsYHQ/s2048/nadan%2Bprawns%2Bfry%2B1.JPG")
+  ]; 
+
+  constructor(){}
 
 }
