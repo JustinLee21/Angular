cb1f7d139e6f3370b4022e03367b608c13f7a545
JustinLee21 <JLLemmons21@gmail.com>
2023-03-29 12:38:23 -0400

    Project: cooking_project: made some updates so able to successfully add an item and the amount to the ingredients. Also, improved syntax of a few components.

--- cooking_project/src/app/recipe/recipe-list/recipe-list.component.ts
+++ cooking_project/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
   @Output() recipeWasSelected = new EventEmitter<Recipe>(); 
 
-  recipe: Recipe[] = [
+  recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', "https://1.bp.blogspot.com/-e-GOmN5ct00/X1a5uEU4HrI/AAAAAAAAYsA/mh_OPlhyyv0RUGcCXTr_hh0W_sAkvf1HwCLcBGAsYHQ/s2048/nadan%2Bprawns%2Bfry%2B1.JPG"),
     new Recipe('A Second Test Recipe', 'This is the second test', "https://1.bp.blogspot.com/-e-GOmN5ct00/X1a5uEU4HrI/AAAAAAAAYsA/mh_OPlhyyv0RUGcCXTr_hh0W_sAkvf1HwCLcBGAsYHQ/s2048/nadan%2Bprawns%2Bfry%2B1.JPG")
   ]; 
